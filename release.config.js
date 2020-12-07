4f2a27d8cff11a997b083676be7bbf7fe967920a
hasparus <hasparus@gmail.com>
2020-12-07 15:05:44 +0100

    💚 configure release branches

--- release.config.js
+++ release.config.js
@@ -4,4 +4,12 @@ module.exports = {
     "@semantic-release/github",
     "@semantic-release/npm",
   ],
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
 };
